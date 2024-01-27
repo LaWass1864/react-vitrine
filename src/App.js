3c3576f082177c80d93977c0223013f17d3319e5
LaWass1864 <amwass4@hotmail.com>
2024-01-27 13:34:22 +0100

    navigation ok

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ const App = () => {
   return (
 
     <BrowserRouter>
-      <Routes>
+   <Routes>
+
+    <Route path='/' element={<Home />} />
+    <Route path='/projet-1' element={<Project1/>} />
+    <Route path='/projet-2' element={<Project2 />} />
+    <Route path='/projet-3' element={<Project3 />} />
+    <Route path='/projet-4' element={<Project4 />} />
+    <Route path='/contact' element={<Contact />} />
+    <Route path='/*' element={<NotFound />} />
+    
    
-      </Routes>
+   </Routes>
     </BrowserRouter>
 
   );
