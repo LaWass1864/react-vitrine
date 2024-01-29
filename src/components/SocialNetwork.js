2d7c3a2734fdaa6321b0fadb7713f2649db80e9e
LaWass1864 <amwass4@hotmail.com>
2024-01-29 17:46:31 +0100

    55:30 animation des logos ok

--- src/components/SocialNetwork.js
+++ src/components/SocialNetwork.js
@@ -3,15 +3,30 @@ import React from 'react';
 const SocialNetwork = () => {
 
     const anim = () => {
-        const icons = document.querySelectorAll(".social-network a")
+        const icons = document.querySelectorAll(".social-network a");
+        
+        // Pour chaque icons de reseau sociaux
+        icons.forEach((link) => {
+            link.addEventListener("mouseover", (e) => {
+                link.style.transform = `translate(${e.offsetX - 20}px, ${e.offsetY - 13}px)`;
+            });
 
-        console.log(icons);
-    }
+            link.addEventListener("mouseleave", () => {
+                link.style.transform = "";
+            })
+        });
+    };
+
+
     return (
         <div className="social-network">
 
             <ul className='content'>
-                <a href="https://www.facebook.com" target='_blank' rel='noopener noreferrer' className='hover' onMouseOver={anim}>
+                <a href="https://www.facebook.com" 
+                target='_blank' 
+                rel='noopener noreferrer' 
+                className='hover' 
+                onMouseOver={anim}>
                     <li>
                         <i className='fab fa-facebook-f' ></i>
                     </li>
