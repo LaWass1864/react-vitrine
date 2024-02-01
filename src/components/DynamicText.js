ce71246df1f41bfa987eb2259c946873b457e7cc
LaWass1864 <amwass4@hotmail.com>
2024-02-01 16:42:31 +0100

    1:06:54

--- src/components/DynamicText.js
+++ src/components/DynamicText.js
@@ -1,44 +1,39 @@
 import React, { useEffect } from "react";
 
+
 const DynamicText = () => {
+  // Hook pour gerer les effets de bord dans les composants
   useEffect(() => {
+    //  endroit dans lequel on va injecter nos lettres
     const target = document.getElementById("text-target");
+    //  Tableau des mots
     let array = ["simple", "clear", "smart", "strong"];
+    //  pour savoir sur quel mot nous sommes
     let wordIndex = 0;
+    // pour savoir sur quelle lettre nous sommes
     let letterIndex = 0;
 
+    // fonction pour injecter des lettres une par une
     const createLetter = () => {
+      //  creer un span
       const letter = document.createElement("span");
+      //  ce span appartient a letter
       target.appendChild(letter);
 
+      //  ce span s'apelle letter + ajout de la classe letter
       letter.classList.add("letter");
+      //  style avec une opacity de 0
       letter.style.opacity = "0";
+      //  animation des lettres 
       letter.style.animation = "anim 5s ease forwards";
-      letter.textContent = array[wordIndex][letterIndex];
+      // notre span va contenir le tableau array et ses index
+      letter.textContent = array[1][3];
 
       setTimeout(() => {
         letter.remove();
       }, 2000);
     };
-
-    const loop = () => {
-      setTimeout(() => {
-        if (wordIndex >= array.length) {
-          wordIndex = 0;
-          letterIndex = 0;
-          loop();
-        } else if (letterIndex < array[wordIndex].length) {
-          createLetter();
-          letterIndex++;
-          loop();
-        } else {
-          letterIndex = 0;
-          wordIndex++;
-          setTimeout(loop, 2000);
-        }
-      }, 80);
-    };
-    loop();
+    createLetter();
   }, []);
 
   return (
@@ -47,6 +42,6 @@ const DynamicText = () => {
       <span id="text-target"></span>
     </span>
   );
-};
+  }
 
 export default DynamicText;
