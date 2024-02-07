e65af0c52103221daa244c634311aeb15905053c
LaWass1864 <amwass4@hotmail.com>
2024-02-07 17:51:14 +0100

    58:14

---
+++ src/components/DynamicText.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DynamicText = () => {
+    return (
+        <div>
+        
+        
+        </div>
+    );
+};
+
+export default DynamicText;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,29 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 import SocialNetwork from '../components/SocialNetwork';
 import Mouse from '../components/Mouse';
+import DynamicText from '../components/DynamicText';
 
 
 // La souris est ici car si nous la mettons dans App.js ca poserai pb
 const Home = () => {
+    {/* la souris est dans ce fichier pour l'appeller dans chaque page */ }
     return (
         <div>
-            {/* la souris est dans ce fichier pour l'appeller dans chaque page */}
             <Mouse />
-            {/*  menu */}
-            <Navigation />
-            <SocialNetwork />
-         
+            <div className="home">
+                {/*  menu */}
+                <Navigation />
+                <SocialNetwork />
+                {/* on met le composant titre */
+                }
+                <div className="home-main">
+                    <div className="main-content">
+                        <h1> Wass Porfolio </h1>
+                        {/* on injectera tout ce qu'on veut dans le DynamicText */}
+                        <h2><DynamicText /></h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
